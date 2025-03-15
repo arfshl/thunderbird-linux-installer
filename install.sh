f030a14f2953e41104ffec48a0f4d7ba8e07e259
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-15 21:15:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing Thunderbird..."
 sudo wget https://ftp.mozilla.org/pub/thunderbird/releases/136.0/linux-x86_64/en-US/thunderbird-136.0.tar.xz -P /opt
 cd /opt
-sudo tar xjvf /opt/thunderbird*.tar.*
+sudo tar xvf /opt/thunderbird*.tar.*
 sudo chmod -R 755 /opt/thunderbird*
 sudo ln -s /opt/thunderbird/thunderbird /usr/local/bin/thunderbird
 wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/installing-thunderbird-linux/thunderbird.desktop -P /usr/local/share/applications
